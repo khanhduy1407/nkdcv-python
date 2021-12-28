538c29409815b9b37fbdda3aff71963ed6508aea
khanhduy1407 <kn145660@gmail.com>
2021-12-28 13:45:30 +0700

    add examples

---
+++ Examples/CornerRectangleExample.py
@@ -0,0 +1,21 @@
+import nkdcv
+from nkdcv.HandTrackingModule import HandDetector
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector()
+
+while True:
+    # Get image frame
+    success, img = cap.read()
+
+    # Find the hand and its landmarks
+    img = detector.findHands(img, draw=False)
+    lmList, bbox = detector.findPosition(img, draw=False)
+    if bbox:
+        # Draw  Corner Rectangle
+        nkdcv.cornerRect(img, bbox)
+
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
---
+++ Examples/StackImageExample.py
@@ -0,0 +1,15 @@
+import nkdcv
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgList = [img, img, imgGray, img, imgGray, img,imgGray, img, img]
+    stackedImg = nkdcv.stackImages(imgList, 3, 0.4)
+
+    cv2.imshow("stackedImg", stackedImg)
+    cv2.waitKey(1)
