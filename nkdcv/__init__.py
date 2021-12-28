9772b44136533b6fc0b9d945f7e2ced1c2708226
khanhduy1407 <kn145660@gmail.com>
2021-12-28 12:35:47 +0700

    add Classification Module

--- nkdcv/__init__.py
+++ nkdcv/__init__.py
@@ -3,3 +3,4 @@ from nkdcv.ColorModule import ColorFinder
 from nkdcv.FPS import FPS
 from nkdcv.PIDModule import PID
 from nkdcv.PlotModule import LivePlot
+from nkdcv.ClassificationModule import Classifier
