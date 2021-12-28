cce18a6fda5b516091f4d1978af4fac7c84f28cd
khanhduy1407 <kn145660@gmail.com>
2021-12-28 12:41:02 +0700

    add Selfy Segmentation Module

--- nkdcv/__init__.py
+++ nkdcv/__init__.py
@@ -3,4 +3,3 @@ from nkdcv.ColorModule import ColorFinder
 from nkdcv.FPS import FPS
 from nkdcv.PIDModule import PID
 from nkdcv.PlotModule import LivePlot
-from nkdcv.ClassificationModule import Classifier
