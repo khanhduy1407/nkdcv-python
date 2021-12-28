931acf984750c18653279c7903ec89026f175d97
khanhduy1407 <kn145660@gmail.com>
2021-12-28 12:30:06 +0700

    add Color and Plot Module

--- nkdcv/__init__.py
+++ nkdcv/__init__.py
@@ -1,3 +1,5 @@
-from nkdcv.Utils import stackImages, cornerRect
+from nkdcv.Utils import stackImages, cornerRect, findContours
+from nkdcv.ColorModule import ColorFinder
 from nkdcv.FPS import FPS
 from nkdcv.PIDModule import PID
+from nkdcv.PlotModule import LivePlot
